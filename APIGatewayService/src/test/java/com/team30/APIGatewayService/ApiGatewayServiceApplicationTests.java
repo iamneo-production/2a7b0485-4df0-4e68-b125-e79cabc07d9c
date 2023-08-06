9a0540c72a9a819e9833be5c4f4df6ae498151f7
subhankarmaji <subhankarmaji500@gmail.com>
2023-08-06 11:18:59 +0000

    added API gateway

---
+++ APIGatewayService/src/test/java/com/team30/APIGatewayService/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team30.APIGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
